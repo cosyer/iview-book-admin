c0250e028314ff5cf9a187e37b0176acca1383d4
chenyu <535509852@qq.com>
2019-04-09 00:43:48 +0800

    fix some bugs

--- README.md
+++ README.md
@@ -9,7 +9,6 @@
 	另外还有主题功能，有多个主题可供选择(store/modules/menu.js) 
 	可以全屏放大缩小
 ## 技术栈
-```js
     // 前端
     1.vue
     2.vue-router
@@ -21,36 +20,35 @@
     1.Node
     2.Eggjs
     3.Mongodb + Mongoose
-```
 
 ## 安装
 ```js
-    $ git clone  https://github.com/cosyer/iview-book-admin.git
-    $ cd iview-book-admin
-    // 前端
-    $ npm i
-    $ npm run dev
-    // 打开地址 http://127.0.0.1:9898
+git clone  https://github.com/cosyer/iview-book-admin.git
+cd iview-book-admin
+// 前端
+npm i
+npm run dev
+// 打开地址 http://127.0.0.1:9898
 
 
-    // 后端  
-    // 确认已经安装mongodb  这里默认配置是 
-    // config.mongoose = {
-    //     url: 'mongodb://127.0.0.1:27017/book',
-    //     options: {},
-    // };
+// 后端  
+// 确认已经安装mongodb  这里默认配置是 
+// config.mongoose = {
+//     url: 'mongodb://127.0.0.1:27017/book',
+//     options: {},
+// };
 
-    // 数据库需要插入一条初始admin的数据
-    //   db.users.insert({
-    //       "userName":"admin",
-    //       "password":"admin",
-    //       "isAdmin": true
-    //   })
-    $ cd server
-    $ npm i 
-    $ npm run dev
-    
-    // 打开地址 http://127.0.0.1:7001/public/web/index.html
+// 数据库需要插入一条初始admin的数据
+//   db.users.insert({
+//       "userName":"admin",
+//       "password":"admin",
+//       "isAdmin": true
+//   })
+cd server
+npm i 
+npm run dev
+
+// 打开地址 http://127.0.0.1:7001/public/web/index.html
 ```
    
  ## 参考
--- src/components/Layout/SearchBox.vue
+++ src/components/Layout/SearchBox.vue
@@ -1,26 +1,43 @@
 <template>
-    <div class="fy-search-header">
-        <Row :gutter="16">
-            <Form ref="form" :model="params" :label-width="labelWidth" label-position="left">
-                <slot :open="isOpen" name="body"></slot>
-            </Form>
-        </Row>
-        <Row>
-            <div class="fy-pull-left">
-                <slot name="action"></slot>
-            </div>
-            <div class="fy-search-footer fy-pull-right">
-                <Button @click="$emit('search')" type="primary">查询</Button>
-                <i class="fy-search-p-r"></i>
-                <Button @click="$emit('reset',$refs.form)" type="ghost">重置</Button>
-                <i class="fy-search-p-r"></i>
-                <span @click="openBody" class="fy-search-header-open">
-                    {{!isOpen?'展开':'收起'}}
-                    <Icon :type="!isOpen?'chevron-down':'chevron-up'"></Icon>
-                </span>
-            </div>
-        </Row>
-    </div>
+  <div class="fy-search-header">
+    <Row :gutter="16">
+      <Form
+        ref="form"
+        :model="params"
+        :label-width="labelWidth"
+        label-position="left"
+      >
+        <slot
+          :open="isOpen"
+          name="body"
+        ></slot>
+      </Form>
+    </Row>
+    <Row>
+      <div class="fy-pull-left">
+        <slot name="action"></slot>
+      </div>
+      <div class="fy-search-footer fy-pull-right">
+        <Button
+          @click="$emit('search')"
+          type="primary"
+        >查询</Button>
+        <i class="fy-search-p-r"></i>
+        <Button
+          @click="$emit('reset',$refs.form)"
+          type="ghost"
+        >重置</Button>
+        <i class="fy-search-p-r"></i>
+        <span
+          @click="openBody"
+          class="fy-search-header-open"
+        >
+          {{!isOpen?'展开':'收起'}}
+          <Icon :type="!isOpen?'chevron-down':'chevron-up'"></Icon>
+        </span>
+      </div>
+    </Row>
+  </div>
 </template>
 
 <script>
